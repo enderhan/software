33eb0c8e8380336e7f0ce3578bb27a3462da73f6
jiuxli@outlook.com <jiuxli@outlook.com>
2014-11-08 18:25:31 +0800

    first version

---
+++ Segmentation.java
@@ -0,0 +1,11 @@
+package GroupName_zero_Segmentation.chinese_segmentation;
+
+public class Segmentation {
+	public  static void main(String[] args)
+	{
+		    Usergui usergui = new Usergui();
+			
+	}
+}
+
+ 
